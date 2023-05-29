d8271dd5e25b7d3e3b0ede7962bc1a292dc512cd
gunlapukha <124670796+gunlapukha@users.noreply.github.com>
2023-05-29 10:47:15 +0700

    Add files via upload

---
+++ tourpoin.js
@@ -0,0 +1,146 @@
+var tourpoint = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "kruaapsorn",
+        "level": 10
+      },
+      "geometry": {
+        "coordinates": [
+          100.50155602561495,
+          13.7554532989041
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "supannigaeatingroom",
+        "level": 6
+      },
+      "geometry": {
+        "coordinates": [
+          100.52746855963454,
+          13.723190174593626
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "ruenmallika",
+        "level": 8
+      },
+      "geometry": {
+        "coordinates": [
+          100.56309729262614,
+          13.725361746382532
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "baankhanitha ",
+        "level": 10
+      },
+      "geometry": {
+        "coordinates": [
+          100.56340682664853,
+          13.741386586095913
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": " attarote",
+        "level": 6
+      },
+      "geometry": {
+        "coordinates": [
+          100.5716640256149,
+          13.737832885237196
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "lukkaithong",
+        "level": 10
+      },
+      "geometry": {
+        "coordinates": [
+          100.51120429262596,
+          13.726858745274527
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "salarattanakosin",
+        "level": 10
+      },
+      "geometry": {
+        "coordinates": [
+          100.49082829262989,
+          13.745438157118059
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "thonkrueng",
+        "level": 9
+      },
+      "geometry": {
+        "coordinates": [
+          100.57684249159047,
+          13.737781589090645
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "phukettownrestaurant ",
+        "level": 8
+      },
+      "geometry": {
+        "coordinates": [
+          100.61204175860325,
+          13.833295774594106
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "kiewkaika",
+        "level": 10
+      },
+      "geometry": {
+        "coordinates": [
+          100.60678682664894,
+          13.797593999598547
+        ],
+        "type": "Point"
+      }
+    }
+  ]
+}
+  
